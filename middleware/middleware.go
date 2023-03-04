3bd942e5b40d2f02ae6c4e252f4bb30471e258e9
unknown <you@example.comkartikshukla2001@gmail.com>
2023-03-04 13:08:02 +0530

    Made functions to getAllTask,CreateTask and TaskComplete

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -29,5 +29,64 @@ var collection *mongo.Collection
 func init(){
 
 	clientOptions :=options.Client().ApplyURI(connectionString)
+
+	client, err :=mongo.Connect(context.TODO(),clientOptions)
+
+	if err !=nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(),nil)
+
+	if err !=nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	fmt.Println("Collection instance created")
+
 }
+
+//Get all the task route
+func GetAllTask(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Context-Type","application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin","*")
+	payload := getAlltask()
+	json.NewEncoder(w).Encode(payload)
+}
+
+//CreateTask create task route
+
+func CreateTask(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Context-Type","application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin","*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers","Content-Type")
+	var task models.ToDoList 
+	_=json.NewDecoder(r.Body).Decode(&task)
+	insertOneTask(task)
+	json.NewEncoder(w).Encode(task)
+}
+
+//TaskComplete Update task route
+
+func TaskComplete(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Context-Type","application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin","*")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("Access-Control-Allow-Headers","Content-Type")
+	
+	params:=mux.Vars(r)
+	taskComplete(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+
+
+
+
+
 
